c5701a708343096b128946845e5ef0c01a6534ff
Karen Mao <karen.mao707@gmail.com>
2015-08-28 16:04:59 -0400

    done

--- public/js/controllers/newcardcontroller.js
+++ public/js/controllers/newcardcontroller.js
@@ -1,20 +1,43 @@
-app.controller('NewCardController', function ($scope, FlashCardsFactory) {
-	$scope.categories = FlashCardsFactory.categories;
-	$scope.newCard = {
-    question: null,
-    category: null,
-    answers: [
-        { text: null, correct: false },
-        { text: null, correct: false },
-        { text: null, correct: false }
-    ]
-	}
-	$scope.postCards = function(newCard) {
-		console.log("postCards", newCard);
-		FlashCardsFactory.postFlashCards(newCard).then(function(card){
-			
-		})
-	}
+app.controller('NewCardController', function($scope, FlashCardsFactory) {
+    $scope.categories = FlashCardsFactory.categories;
+    $scope.newCard = {
+        question: null,
+        category: null,
+        answers: [{
+            text: null,
+            correct: false
+        }, {
+            text: null,
+            correct: false
+        }, {
+            text: null,
+            correct: false
+        }]
+    }
+    $scope.submit = function(newCard, form) {
+        if (!form.$error.maxlength) {
+            console.log("postCards", newCard);
+            FlashCardsFactory.postFlashCards(newCard).then(function(card) {
+                console.log("moongoose", card);
+            }).finally(function() {
+                $scope.newCard = {
+                    question: null,
+                    category: null,
+                    answers: [{
+                        text: null,
+                        correct: false
+                    }, {
+                        text: null,
+                        correct: false
+                    }, {
+                        text: null,
+                        correct: false
+                    }]
+                }
+            })
+
+        } else console.log("submission prevented");
+    }
 
 
 
